de8dfddca469d0817970e80346f380bc34cbfea4
CG-Tespy <gbcabreracgt@gmail.com>
2022-09-01 05:12:09 -0800

    Fixed regex glitch that made it so LB tags didn't work right

--- WordWrapCore/Shared/_Regexes.ts
+++ WordWrapCore/Shared/_Regexes.ts
@@ -1,4 +1,6 @@
-export let doubleSpaces: RegExp = /\s\s+/gm;
+export let doubleSpaces: RegExp = / [^\S\r\n][^\S\r\n]+/gm;
+// ^Had to do this weird double-negative so it wouldn't treat newlines
+// as whitespace
 export let newlines: RegExp = /\n+/gm;
 export let notWhitespace: RegExp = /\S+/gm;
 export let spaceAsSeparator: RegExp = / /gm;
@@ -10,6 +12,7 @@ export let doubleQuotes: RegExp = /"/gm;
 
 export let noWrapTag: RegExp = /<noWrap>\s?/gmi;
 export let disableNametagScanTag: RegExp = /<disableNametagScan>\s?/gmi;
+export let disableCascadingUnderflowTag: RegExp = /<disableCascadingUnderflow>\s?/gmi;
 
 export let boldMarkers: RegExp = /\u001bMSGCORE\[1\]/gmi;
 export let italicsMarkers: RegExp = /\u001bMSGCORE\[2\]/gmi;
